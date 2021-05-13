60165bb17f548d765d13a358075cbc5682cbfab6
Patrick Kuen <patrick.kuen11@gmail.com>
2021-05-13 12:34:39 +0200

    feat(android): support fetching all available exif attributes we define each tag, which we are able to fetch

--- android/src/main/kotlin/com/cloudacy/native_exif/NativeExifPlugin.kt
+++ android/src/main/kotlin/com/cloudacy/native_exif/NativeExifPlugin.kt
@@ -2,13 +2,11 @@ package com.cloudacy.native_exif
 
 import android.media.ExifInterface
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** NativeExifPlugin */
 class NativeExifPlugin: FlutterPlugin, MethodCallHandler {
@@ -52,6 +50,59 @@ class NativeExifPlugin: FlutterPlugin, MethodCallHandler {
           }
 
           result.success(exif.getAttribute(tag))
+        }
+        "getAttributes" -> {
+          val id = call.argument<Int>("id")
+
+          if (id == null) {
+            result.error("BAD_ARGUMENTS", "Bad arguments were given to this method.", null)
+            return
+          }
+
+          val exif = interfaces[id]
+
+          if (exif == null) {
+            result.error("NOT_FOUND", "Exif with given id was not found in memory", null)
+            return
+          }
+
+          /// As all relevant fields are private for the exif interface, we have to list all common tags here. To be extended..
+          var tags = arrayOf(
+            ExifInterface.TAG_APERTURE_VALUE,
+            ExifInterface.TAG_DATETIME,
+            ExifInterface.TAG_DATETIME_DIGITIZED,
+            ExifInterface.TAG_DATETIME_ORIGINAL,
+            ExifInterface.TAG_EXPOSURE_TIME,
+            ExifInterface.TAG_F_NUMBER,
+            ExifInterface.TAG_FLASH,
+            ExifInterface.TAG_FOCAL_LENGTH,
+            ExifInterface.TAG_GPS_ALTITUDE,
+            ExifInterface.TAG_GPS_ALTITUDE_REF,
+            ExifInterface.TAG_GPS_DATESTAMP,
+            ExifInterface.TAG_GPS_LATITUDE,
+            ExifInterface.TAG_GPS_LATITUDE_REF,
+            ExifInterface.TAG_GPS_LONGITUDE,
+            ExifInterface.TAG_GPS_LONGITUDE_REF,
+            ExifInterface.TAG_GPS_PROCESSING_METHOD,
+            ExifInterface.TAG_GPS_TIMESTAMP,
+            ExifInterface.TAG_IMAGE_LENGTH,
+            ExifInterface.TAG_IMAGE_WIDTH,
+            ExifInterface.TAG_ISO_SPEED_RATINGS,
+            ExifInterface.TAG_MAKE,
+            ExifInterface.TAG_MODEL,
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.TAG_SUBSEC_TIME,
+            ExifInterface.TAG_SUBSEC_TIME_ORIGINAL,
+            ExifInterface.TAG_SUBSEC_TIME_DIGITIZED,
+            ExifInterface.TAG_WHITE_BALANCE
+          )
+
+          val attributeMap = HashMap<String, String>()
+
+          for (tag in tags)
+            exif.getAttribute(tag)?.let { attributeMap[tag] = it }
+
+          result.success(attributeMap)
         }
         "close" -> {
           val id = call.argument<Int>("id")

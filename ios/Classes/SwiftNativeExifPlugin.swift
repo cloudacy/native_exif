a977f18b720a277e118c56f52aaca3cd47c7823a
Patrick Kuen <patrick.kuen11@gmail.com>
2021-05-15 21:14:57 +0200

    feat(ios): implement setAttribute method

--- ios/Classes/SwiftNativeExifPlugin.swift
+++ ios/Classes/SwiftNativeExifPlugin.swift
@@ -1,29 +1,50 @@
 import Flutter
 import UIKit
 
-public class SwiftNativeExifPlugin: NSObject, FlutterPlugin {
-  private var interfaces: [Int: CFDictionary] = [:]
-  private var classId = 0
+struct NativeExifError: Error {
+  let code: String
+  let message: String
+  let details: Any?
   
-  struct NativeExifError: Error {
-    let code: String
-    let message: String
-    let details: Any?
-    
-    init(code: String, message: String, details: Any?) {
-      self.code = code
-      self.message = message
-      self.details = details
-    }
-    
-    static func badArguments(message: String, details: Any?) -> NativeExifError {
-      return NativeExifError(code: "BAD_ARGUMENTS", message: message, details: details)
-    }
-    
-    func toFlutterError() -> FlutterError {
-      return FlutterError(code: self.code, message: self.message, details: self.details)
-    }
+  init(code: String, message: String, details: Any?) {
+    self.code = code
+    self.message = message
+    self.details = details
   }
+  
+  static func badArguments(message: String, details: Any?) -> NativeExifError {
+    return NativeExifError(code: "BAD_ARGUMENTS", message: message, details: details)
+  }
+  
+  func toFlutterError() -> FlutterError {
+    return FlutterError(code: self.code, message: self.message, details: self.details)
+  }
+}
+
+private class NativeExifInterface {
+  public var path: String
+  
+  init(path: String) {
+    self.path = path
+  }
+  
+  func getMetadata() throws -> NSDictionary {
+    let url = NSURL(fileURLWithPath: path)
+    guard let source = CGImageSourceCreateWithURL(url, nil) else {
+      throw NativeExifError(code: "SRC_ERROR", message: "Error while creating source", details: nil)
+    }
+    
+    guard let metadata = CGImageSourceCopyPropertiesAtIndex(source, 0, nil) else {
+      throw NativeExifError(code: "READ_ERROR", message: "Error while reading metadata from source", details: nil)
+    }
+    
+    return metadata as NSDictionary
+  }
+}
+
+public class SwiftNativeExifPlugin: NSObject, FlutterPlugin {
+  private var interfaces: [Int: NativeExifInterface] = [:]
+  private var classId = 0
   
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "native_exif", binaryMessenger: registrar.messenger())
@@ -48,8 +69,10 @@ public class SwiftNativeExifPlugin: NSObject, FlutterPlugin {
       throw NativeExifError(code: "NOT_FOUND", message: "No ExifInterface was found with given id", details: nil)
     }
     
-    guard let exif = (interface as NSDictionary)["{Exif}"] as? NSDictionary else {
-      throw NativeExifError(code: "NO_EXIF_DATA", message: "No Exif data was found on this image.", details: nil)
+    let metadata = try interface.getMetadata()
+    
+    guard let exif = metadata["{Exif}"] as? NSDictionary else {
+      throw NativeExifError(code: "READ_ERROR", message: "No Exif data was found on this image.", details: nil)
     }
     
     return exif
@@ -61,19 +84,9 @@ public class SwiftNativeExifPlugin: NSObject, FlutterPlugin {
       classId += 1
       let id = classId
       let path = call.arguments as! String
-      let url = NSURL(fileURLWithPath: path)
-      guard let source = CGImageSourceCreateWithURL(url, nil) else {
-        result(FlutterError(code: "SRC_ERROR", message: "Error while creating source", details: nil))
-        return
-      }
-      
-      guard let metadata = CGImageSourceCopyPropertiesAtIndex(source, 0, nil) else {
-        result(FlutterError(code: "READ_ERROR", message: "Error while reading metadata from source", details: nil))
-        return
-      }
-      
-      interfaces[id] = metadata
-      
+
+      interfaces[id] = NativeExifInterface(path: path)
+
       result(id)
     case "getAttribute":
       do {
@@ -95,8 +108,54 @@ public class SwiftNativeExifPlugin: NSObject, FlutterPlugin {
       do {
         let arguments = try getArguments(call.arguments)
         let exif = try getExifData(arguments)
-        
+                
         result(exif)
+      } catch let error as NativeExifError {
+        result(error.toFlutterError())
+      } catch {
+        result(FlutterError())
+      }
+    case "setAttribute":
+      do {
+        let arguments = try getArguments(call.arguments)
+        guard let id = arguments["id"] as? Int else {
+          throw NativeExifError.badArguments(message: "id field must be of type integer.", details: nil)
+        }
+        
+        guard let interface = interfaces[id] else {
+          throw NativeExifError(code: "NOT_FOUND", message: "No ExifInterface was found with given id", details: nil)
+        }
+        
+        let url = NSURL(fileURLWithPath: interface.path)
+        guard let source = CGImageSourceCreateWithURL(url, nil), let uniformTypeIdentifier = CGImageSourceGetType(source) else {
+          throw NativeExifError(code: "SRC_ERROR", message: "Error while creating source", details: nil)
+        }
+        
+        guard let destination = CGImageDestinationCreateWithURL(url, uniformTypeIdentifier, 1, nil) else {
+          throw NativeExifError(code: "WRITE_ERROR", message: "Error while creating destination for writing metadata", details: nil)
+        }
+        
+        guard var metadata = try interface.getMetadata() as? [String: AnyObject] else {
+          throw NativeExifError(code: "READ_ERROR", message: "Metadata could not be converted to mutable dictionary", details: nil)
+        }
+        
+        guard var exif = metadata["{Exif}"] as? [String: AnyObject] else {
+          throw NativeExifError(code: "READ_ERROR", message: "No Exif data was found on this image.", details: nil)
+        }
+        
+        guard let tag = arguments["tag"] as? String else {
+          throw NativeExifError.badArguments(message: "tag field must be of type string.", details: nil)
+        }
+
+        exif[tag] = arguments["value"] as AnyObject?
+        metadata["{Exif}"] = exif as AnyObject
+        
+        CGImageDestinationAddImageFromSource(destination, source, 0, metadata as CFDictionary)
+        guard CGImageDestinationFinalize(destination) else {
+          throw NativeExifError(code: "WRITE_ERROR", message: "Error while finalizing destination for writing metadata", details: nil)
+        }
+        
+        result(nil)
       } catch let error as NativeExifError {
         result(error.toFlutterError())
       } catch {
